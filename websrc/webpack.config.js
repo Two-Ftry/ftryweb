e48f556f68d0320dd72c586f43de4faef7e6272c
suifengfengye <1527261080@qq.com>
2017-01-01 14:46:27 +0800

    提交

--- websrc/webpack.config.js
+++ websrc/webpack.config.js
@@ -4,47 +4,29 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 //util
 var readEntrys = require('./webpack-util/readEntrys');
 
+//变量
 var devPath = path.join(__dirname, './').replace(/\\/g, "/");
-console.log('devPath', devPath);
+
+
 //读取入口文件
 var entryFiles = readEntrys.getEntryFiles();
 var entryPort = {};
 entryFiles.forEach(function(filepath){
-  console.log('filepath', filepath);
-  // filepath = path.normalize(filepath);
-  console.log('filepath2', filepath);
   var dir = filepath.replace(devPath, '');
-  console.log('dir', dir);
   var key = path.dirname(dir);
   entryPort[key] = [];
   entryPort[key].push(filepath);
 });
-console.log(entryPort);
 
+//基础配置
 var config = {
   entry: entryPort,
-  // entry: {
-  //   'business/customer': ['./websrc/business/customer/customer.entry.js'],
-  //   'business/org': ['./websrc/business/org/org.entry.js']
-  // },
   output: {
     path: path.resolve(__dirname, "../public"),
     publicPath: "/ftryweb/",
     filename: "[name].js"
   },
   plugins: [
-    // new HtmlWebpackPlugin({
-    //   title: 'This is greate',
-    //   template: './websrc/common/template.html',
-    //   filename: 'business/customer.html',
-    //   chunks: ['business/customer']
-    // }),
-    // new HtmlWebpackPlugin({
-    //   title: 'org',
-    //   template: './websrc/common/template.html',
-    //   filename: 'business/org.html',
-    //   chunks: ['business/org']
-    // })
   ]
 };
 
