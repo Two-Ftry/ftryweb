131570dac6ad2166ad30c9de7884b71b73d48d21
jfhuang <jfhuang@jfhuangdeMacBook-Air.local>
2017-02-06 08:03:52 +0800

    reg page dev

--- websrc/business/org/components/content/Register.vue
+++ websrc/business/org/components/content/Register.vue
@@ -1,13 +1,25 @@
 <template lang="html">
   <div class="reg-box">
     <div class="reg-inner-box">
-      注册页面
+      <div class="reg-form-box">
+        <div class="">
+          <fw-input icon="geren" placeholder="请输入用户名"></fw-input>
+        </div>
+        <div class="">
+          <fw-input placeholder="请输入密码"></fw-input>
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import FwInput from 'ui/form/FwInput';
 export default {
+  components:{
+    FwInput
+  }
 }
 </script>
 
@@ -16,7 +28,7 @@ export default {
 .reg-box{
   height: 100%;
   background: url('~assets/images/rooster.jpg') top left no-repeat;
-  background-size: 100%;
+  background-size: 100% 100%;
   .reg-inner-box{
       width: $boxWidth;
       margin: 0 auto;
--- websrc/ui/form/FwInput.vue
+++ websrc/ui/form/FwInput.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="fw-input-box">
-    <input type="text" name="" value="" />
+    <input type="text" name="" value="" :placeholder="placeholder" :style="inputStyle"/>
     <i v-if="icon" class="ftryweb" :class="['icon-' + icon]"></i>
   </div>
 </template>
@@ -17,6 +17,16 @@ export default {
     iconPosition:{
       type: String,
       default: 'right'
+    },
+    placeholder: String
+  },
+  computed: {
+    inputStyle(){
+      let o = {};
+      if(this.icon){
+        o['padding-right'] = '30px';
+      }
+      return o;
     }
   }
 }
@@ -25,12 +35,20 @@ export default {
 <style lang="sass">
   .fw-input-box{
     position: relative;
+    display: inline-block;
     input{
       display: inline-block;
       height: 32px;
       line-height: 32px;
+      padding: 0 10px;
       border-radius: 4px;
-      border: 1px solid #fff;
+      // border: 1px solid #fff;
+      border: 2px solid #dce4ec
+    }
+    i{
+      position: absolute;
+      top: 5px;
+      right: 10px;
     }
   }
 </style>
